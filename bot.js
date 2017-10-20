fed58575f61b1d91774ea82833d1b77d3b167635
snoffelhd <32904082+snoffelhd@users.noreply.github.com>
2017-10-20 22:59:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,2 @@
-var bot = require("discord-music-bot");
- 
-var serverName = "🕺 HYPER SERVER 🕺 (Aufbau)";
-var textChannelName = "admim";
-var voiceChannelName = "admim";
-var aliasesFile = "README.MD";
-var botToken = "MzcwMjAyNTM0NzU0NjQ4MDY0.DMvk0g.dfMFVKZ4R7MZu6jGNZS6Boe8mEY";
- 
-client.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
-client.setYoutubeKey("AIzaSyD_oAhXdRMEZSu6-U0CwAK0NiygxBzOubk");
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
